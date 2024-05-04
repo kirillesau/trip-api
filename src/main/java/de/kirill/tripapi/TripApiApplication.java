213561f9fd0573c3cab6d98d720ba7521e0be7fb
kirill.esau <kirill.esau@adesso.de>
2024-05-04 20:30:38 +0200

    Init commit

---
+++ src/main/java/de/kirill/tripapi/TripApiApplication.java
@@ -0,0 +1,13 @@
+package de.kirill.tripapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripApiApplication.class, args);
+    }
+
+}
